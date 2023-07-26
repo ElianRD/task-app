11bd1b024fc96f62c968cd21c01235be6f238e96
Elian <elianencarnacion03@gmail.com>
2023-07-26 16:51:04 -0400

    tercer commit

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 document.getElementById("form").addEventListener("submit", saveTask);
 
+
+
 function saveTask(e){
     let title = document.querySelector(".form__title").value
     let description = document.querySelector(".form__description").value
@@ -22,16 +24,19 @@ function saveTask(e){
     getTask()
     title.innerHTML = ""
     description.innerHTML= ""
+    document.getElementById("form").reset();
     e.preventDefault()
 }
+
+
 function getTask(){
     let blockTaks = document.querySelector(".tasks")
     let tasks = JSON.parse(localStorage.getItem("task"))
     blockTaks.innerHTML = "";
     let allTask = []
-    console.log(tasks)
-    tasks.forEach(element => {
-        console.log(element)
+ 
+    tasks.forEach((element,index) => {
+
         
         let container = document.createElement("div")
         container.classList.add("task-container")
@@ -41,10 +46,18 @@ function getTask(){
         description.classList.add("task-container__description")
         let deleteBoton = document.createElement("button")
         deleteBoton.textContent = "Eliminar";
-        deleteBoton.classList.add("task-container__delete")
+        deleteBoton.id = index
+        deleteBoton.onclick= function() {
+            deleteTask(index); // Llamamos a la función deleteTask y pasamos el índice como argumento
+          };
+        
         let editBoton = document.createElement("button")
         editBoton.textContent = "editar";
-        editBoton.classList.add("task-container__delete")
+        editBoton.onclick = "deletetask(index)"
+        editBoton.classList.add("task-container__edit")
+        editBoton.onclick= function() {
+            editTask(index); // Llamamos a la función deleteTask y pasamos el índice como argumento
+          };
 
         title.textContent = element.title
         description.textContent = element.description
@@ -54,4 +67,24 @@ function getTask(){
 
     });
     blockTaks.append(...allTask)
+}
+
+function deleteTask(index) {
+    let tasks = JSON.parse(localStorage.getItem("task"))
+    tasks.splice(index,1)
+    localStorage.setItem("task",JSON.stringify(tasks))
+    let title = document.querySelector(".form__title")
+    title.value = "idid"
+    getTask()
+
+}
+
+
+function editTask(index) {
+    let tasks = JSON.parse(localStorage.getItem("task"))
+    let title = document.querySelector(".form__title")
+    let description = document.querySelector(".form__description")
+    title.value = tasks[index].title
+    description.value = tasks[index].description
+
 }
